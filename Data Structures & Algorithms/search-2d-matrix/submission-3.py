a576ac84cff0262dfcbfe7243905c7ac661371e3
YangQF2002 <123465999+YangQF2002@users.noreply.github.com>
2026-09-15 11:16:32 +0800

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.py
@@ -0,0 +1,32 @@
+# No flattening version 
+
+
+# 2D index -> convert to 1D index 
+# Index: (ri, ci) -> ri * num_cols + ci 
+
+# 1D index -> recover 2D index 
+# 1D index // num_cols -> ri 
+# 1D index % num_cols -> ci
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        r = len(matrix)
+        c = len(matrix[0])
+
+        low = 0 
+        high = (r - 1) * c + (c - 1)
+
+        while low <= high: 
+            mid = (low + high) // 2
+            mid_r = mid // c 
+            mid_c = mid % c 
+            mid_val = matrix[mid_r][mid_c]
+            
+            if mid_val == target: 
+                return True 
+            elif mid_val > target: 
+                high = mid - 1 
+            else: 
+                low = mid + 1 
+
+        return False 
